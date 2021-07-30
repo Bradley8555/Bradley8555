cd608f0574e7a86eda2bea98452ce1bff0eb901e
Bradley8555 <53471848+Bradley8555@users.noreply.github.com>
2021-07-30 14:15:43 +0300

    Add files via upload

---
+++ asst.java
@@ -0,0 +1,30 @@
+Object deserialize(byte [] stream) 
+{
+	Constructor aConstructor; 
+	read class_name and class_handle;
+	if (class_information == null) aConstructor = lookup(class_handle);
+	else {
+			Class cl = Class.forName(class_name);
+			read number (n) of instance variables 
+			Class parameterTypes[]= new Class[n];
+			for (int i=0 to n-1) 
+			{
+				read name and class_name of instance variable i
+				parameterTypes[i] = Class.forName(class_name);
+			}
+			aConstructor = cl.getConstructor(parameterTypes);
+			map(aConstructor, class_handle);
+		}
+	if (next item in stream is object_handle) o = lookup(object_handle);
+	else {
+			Object args[] = new Object[n];
+			for (int i=0 to n-1) {
+				if (next item in stream is primitive) args[i] = read value
+				else args[i]  = deserialize(//rest of stream)
+		 		}
+			Object o = cnew.newInstance(args);
+			read object_handle from stream
+			map(object, object_handle)
+			return o;
+		}
+}
---
+++ intervals.java
@@ -0,0 +1,35 @@
+package com.company;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+            int [][] intervals = {{0, 1}, {3, 5}, {7, 9}, {6, 10}, {3, 12}};
+            if (intervals.length <= 1) {
+                System.out.println(Arrays.toString(intervals));
+            }
+            Arrays.sort(intervals, (arr1, arr2) -> Integer.compare(arr1[0], arr2[0]));
+            List<int[]> output_arr = new ArrayList();
+            int[] current_interval = intervals[0];
+            output_arr.add(current_interval);
+            for (int[] interval : intervals) {
+                int current_begin = current_interval[0];
+                int current_end = current_interval[1];
+                int next_begin = interval[0];
+                int next_end = interval[1];
+                if (current_end >= next_begin) {
+                    current_interval[1] = Math.max(current_end, next_end);
+                } else {
+                    current_interval = interval;
+                    output_arr.add(current_interval);
+                }
+
+            }
+            System.out.println(Arrays.deepToString(output_arr.toArray(new int[output_arr.size()][])));
+
+
+
+
+    }
+}
